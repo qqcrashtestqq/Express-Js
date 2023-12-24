390285095fb848100f14d35dbd05180795279b83
MSI <artemlcenko@gmail.com>
2023-12-24 18:45:37 +0200

    creating project

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
+const homeRouts = require('./routes/home');
+const coursesRouts = require('./routes/courses');
+const addRoutes = require('./routes/add');
 
 const hbs = exphbs.create({
   defaultLayout: 'main',
@@ -13,30 +16,11 @@ app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(express.static('public'));
+app.use('/',homeRouts);
+app.use('/courses',coursesRouts);
+app.use('/add',addRoutes);
 
-// routs home page
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Главная страница',
-    isHome: true
-  });
-});
 
-// routs page courses
-app.get('/courses', (req, res) => {
-  res.render('courses', {
-    title: 'Страница с курсами',
-    isCourses: true
-  });
-});
-
-//routs page add courses
-app.get('/add', (req, res) => {
-  res.render('add', {
-    title: 'Добавить курс',
-    isAdd: true
-  });
-});
 
 const PORT = process.env.PORT || 3000;
 
